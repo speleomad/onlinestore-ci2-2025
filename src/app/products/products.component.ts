dfab0f48da1265a34d1a5a0f5d6a14523daabe3d
arbitrontunisie <arbitrontunisie@gmail.com>
2025-02-11 13:35:01 +0100

    atelier2

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../shared/product';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products!:Product[];
+  constructor(private prodductService:ProductService){}
+  
+  ngOnInit(): void {
+     this.products=this.prodductService.getProducts();  
+  }
+
+}
